af0a551c7c85e2e7eab9eb6bc26bc55f0abf7f77
Peter Vegh <pvegh@posteo.uk>
2022-07-22 13:45:30 +0100

    Fix #20
    Fix typos and format to black.

--- examples/labels_on_a4_paper/labels_on_a4_paper.py
+++ examples/labels_on_a4_paper/labels_on_a4_paper.py
@@ -3,12 +3,14 @@ from blabel import LabelWriter
 label_writer = LabelWriter(
     "item_template.html", items_per_page=1, default_stylesheets=("style.css",)
 )
-records = list();
+records = list()
 
-for k in range(1,10):
-    string_sampleid = "s0" + str(k);
-    string_heading = "Heading " + str(k);
-    string_sample = "Sample" + str(k);
-    records.append(dict(sample_id=string_sampleid,heading=string_heading,sample=string_sample));
+for k in range(1, 10):
+    string_sampleid = "s0" + str(k)
+    string_heading = "Heading " + str(k)
+    string_sample = "Sample" + str(k)
+    records.append(
+        dict(sample_id=string_sampleid, heading=string_heading, sample=string_sample)
+    )
 
 label_writer.write_labels(records, target="labels_on_a4_paper.pdf", base_url=".")
